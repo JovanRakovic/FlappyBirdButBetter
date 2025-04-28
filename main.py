75577d570cb5aa2d7533fa8d71ac34a91967cb1c
Jovan Rakovic <jovanrakovic1456@gmail.com>
2025-04-28 22:40:14 +0200

    Sliding Images
    Added omnidirectional sliding images to be used for the background and floor

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import pygame
 from sys import exit
+from sliding_image import SlidingImage
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 
+dt = .016 # DeltaTime, used to keep movement and animation time based, instead of frame-rate based as frame rate can vary.
+
+background = SlidingImage('images/background_temp.png',(0,-50),-40,.75)
+floor = SlidingImage('images/floor_temp.png',(0,670),-145,.7)
+
 while True:
     # poll for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() # Uninitilizes pygame
-            exit()        # Exits the application without executing the code below 
+            exit()        # Exits the application without executing the code below
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("lightblue")
-
+    background.update(screen, dt)
+    floor.update(screen, dt)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    dt = float(clock.tick(60))/1000.0  # limits FPS to 60 and assigns a 0 to 1 value to dt
